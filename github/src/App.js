0b4fce025524e6d20a7b3534fd6802ff823fbf7c
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-08-04 17:21:15 -0400

    mvp

--- github/src/App.js
+++ github/src/App.js
@@ -2,35 +2,34 @@ import React from 'react';
 import './App.css';
 import Axios from 'axios';
 
+let one = 'https://api.github.com/users/ginxy13th';
+let two = 'https://api.github.com/users/ginxy13th/followers';
 
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
       cards: [],
+      followers: []
     }
   }
 
   componentDidMount() {
-    Axios.get('https://api.github.com/users/ginxy13th')
-    .then(response => {
-      console.log(response)
+    const requestOne = Axios.get(one);
+    const requestTwo = Axios.get(two);
+    Axios.all([requestOne, requestTwo])
+    .then(Axios.spread((...responses) => {
+      const responseOne = responses[0]
+      const responseTwo = responses[1]
       this.setState({
-      cards: [response.data],
-      });
-    })
+        cards: [responseOne.data],
+        followers: responseTwo.data
+      })
+    }))
     .catch(err => {
       console.log(err)
     }) 
-    // Axios.get('https://api/github/users/ginxy13th/followers')
-    // .then(res => {
-    //   this.setState({
-    //     followers: [res.data]
-    //   })
-    // })
-    // .catch(err => {
-    //   console.log(err)
-    // })
+    
   }
 
   // componentDidUpdate(previousProps, previousState) {
@@ -46,43 +45,44 @@ class App extends React.Component {
   // }
 
   render(){
+
+  console.log(this.state.followers)
     return (
       
     <div className="App">
       <h1>GitHub Profile Cards</h1>
-      <div>
-      {this.state.cards.map(user => (
-        <div>
-        <img src={user.avatar_url} alt='avatar' />
-        <div>
-          <h3>{user.name}</h3>
-          <p>Location: {user.location}</p>
-          <p>Bio: {user.bio}</p>
-          <p>GitHub Profile:</p>
-          <a href={user.html_url}>{user.html_url}</a>
-          <p>Followers: {user.followers}</p>
-          <p>Following: {user.following}</p>
-       </div>
-      </div> 
-      ))}
+      <div className="BigContainer">
+        <div className="SmallContainer">
+        {this.state.cards.map(user => (
+          <div className="Card">
+            <img src={user.avatar_url} alt='avatar' />
+            <div key={user.id} className="Info" >
+              <h3>{user.name}</h3>
+              <p>Location: {user.location}</p>
+              <p>Bio: {user.bio}</p>
+              <p>GitHub Profile:</p>
+              <a href={user.html_url}>{user.html_url}</a>
+              <p>Followers: {user.followers}</p>
+              <p>Following: {user.following}</p>
+            </div>
+          </div>
+        ))}
+        </div>
+        <div className="SmallContainer">
+        {this.state.followers.map(user => (
+          <div className="Card">
+            <img src={user.avatar_url} alt='avatar' />
+            <div className="Info" >
+              <h3>{user.login}</h3>
+              <p>GitHub Profile:</p>
+              <a href={user.html_url}>{user.html_url}</a>
+            </div>
+          </div>
+        ))}
+        </div>
       </div>
-      {/* <div>
-      {this.state.followers.map(user => (
-        <div>
-        <img src={user.avatar_url} alt='avatar' />
-        <div>
-          <h3>{user.name}</h3>
-          <p>Location: {user.location}</p>
-          <p>Bio: {user.bio}</p>
-          <p>GitHub Profile:</p>
-          <a href={user.html_url}>{user.html_url}</a>
-          <p>Followers: {user.followers}</p>
-          <p>Following: {user.following}</p>
-       </div>
-      </div>
-      ))}
-      </div> */}
     </div>
+    
     )
   }
 }
