0274c6eedeee85a872b87f72bd98f4ca0a29374b
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-08-04 14:34:12 -0400

    working

--- github/src/App.js
+++ github/src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
+// import GitHubForm from './Components/githubform.js';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      cards: [],
+    }
+  }
+
+  componentDidMount() {
+    Axios.get('https://api.github.com/users/ginxy13th/')
+    .then(response => {
+      console.log(response)
+      this.setState({
+      cards: response.data.data
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    }) 
+  }
+
+  // componentDidUpdate(previousProps, previousState) {
+  //   if (previousState.cards !== this.props.cards){
+  //     console.log(this.state.cards)
+  //   }
+  //   if (previousProps.cards.username !== this.props.cards.username){
+  //     console.log(this.state.cards)
+  //     this.setState({
+  //       username: ''
+  //     })
+  //   }
+  // }
+
+  render(){
+    return (
+      
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub Profile Cards</h1>
+      {this.state.cards.map(user => (
+        <div>
+        <img src={user.avatar_url} alt='avatar' />
+        <div>
+          <h3>{user.name}</h3>
+          <p>{user.login}</p>
+          <p>{user.location}</p>
+          <p>{user.bio}</p>
+          <p>{user.profile}</p>
+          <a href={user.html_url}>{user.html_url}</a>
+          <p>{user.followers_url}</p>
+          <p>{user.following_url}</p>
+       </div>
+      </div> 
+      ))}
     </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ github/src/Components/githubform.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+
+// const GitHubForm = user => {
+
+//         return (
+//             // <form>
+//                 <div>
+//                     <img src={user.avatar_url} alt='avatar'></img>
+//                     <div>
+//                         <h3>{user.name}</h3>
+//                         <p>{user.login}</p>
+//                         <p>{user.location}</p>
+//                         <p>{user.bio}</p>
+//                         <p>{user.profile}</p>
+//                         <a href>{user.html_url}>{user.html_url}</a>
+//                         <p>{user.followers_url}</p>
+//                         <p>{user.following_url}</p>
+//                     </div>
+//                 </div>
+//             // </form>
+//         )
+// }
+// export default GitHubForm
